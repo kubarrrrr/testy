1b5c53752ecee01e85f2d67f1d7011018e809696
Kubbek88 <97827305+Kubbek88@users.noreply.github.com>
2022-04-21 13:12:16 +0200

    t

---
+++ automation_selenium02stackoverflow2.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+# from selenium.webdriver.chrome.options import Options
+
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+
+
+s = Service('C:/chromedriver.exe')
+browser = webdriver.Chrome(service=s)
+url='https://stackoverflow.com/'
+browser.get(url)
+
+sleep(3)
+
+
+def cookies2():
+    return browser.find_element(By.XPATH, "/html/body/div[4]/div/button[1]").click()
+
+# cookies = browser.find_element(By.className, "flex--item s-btn s-btn__primary js-accept-cookies js-consent-banner-hide").click()
+cookies2()
+search = browser.find_element(By.NAME, "q")
+search.send_keys("tekst", Keys.ENTER)
+
+# sleep(2)
+cookies2()
+sleep(3)
+browser.close()
+print("udało się!!!!")
