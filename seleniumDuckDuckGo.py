8abd822368247c06a57cd15e5b2c5825d554a1aa
Kubbek88 <97827305+Kubbek88@users.noreply.github.com>
2022-04-21 09:15:02 +0200

    selenium

---
+++ seleniumDuckDuckGo.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
+s = Service('C:/chromedriver.exe')
+browser = webdriver.Chrome(service=s)
+url='https://www.duckduckgo.com/'
+browser.get(url)
+sleep(2)
+main = browser.find_element(By.XPATH, '/html/body/div/div[2]/div/div[3]/div[2]/div/div[3]/div[2]/a').click
