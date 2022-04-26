ce469f47a0e926a0775a4ac977a3c8f2c49cddfc
kubarrrrr <103730599+kubarrrrr@users.noreply.github.com>
2022-04-26 23:59:08 +0200

    dodane z CMD

---
+++ testy/testyTUT/multitest/test_multiple_single.py
@@ -0,0 +1,11 @@
+def test_type():
+    assert type(1 + 2) is int
+
+def test_add_int():
+    assert 5 + 2 == 7
+
+def test_string():
+    assert 'u' in 'sun'
+
+def test_add_string():
+    assert ('sunny' + 'day') == 'sunnyday'
