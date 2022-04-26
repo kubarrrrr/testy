476dd922934cc6672e239648999bbb0c381a0326
kubarrrrr <103730599+kubarrrrr@users.noreply.github.com>
2022-04-26 14:51:07 +0200

    a

---
+++ testy1/POM Selenium/POM_automation_selenium02stackoverflow2.py
@@ -0,0 +1,3 @@
+url = 'https://stackoverflow.com/'
+cookies = "/html/body/div[4]/div/button[1]"
+search = 'q'
---
+++ testy1/POM Selenium/POM_test_JakubRuszkowski_polska_jazda.py
@@ -0,0 +1,4 @@
+strona = 'http://zalo001.polskajazda.pl/Porady/Opinie-o-motocyklach'
+sterownik = 'C:/geckodriver.exe'
+odnosnik = '/html/body/div[5]/div[4]/a[3]'
+url2 = 'http://www.polskajazda.pl/Porady/Opinie-o-motocyklach/Derbi/Senda-DRD-SM'
