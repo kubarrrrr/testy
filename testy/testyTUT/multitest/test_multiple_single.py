315e73307fe77baa9d2aafd4c8296dc652c9bc82
kubarrrrr <103730599+kubarrrrr@users.noreply.github.com>
2022-04-27 10:33:06 +0200

    api

--- testy/testyTUT/multitest/test_multiple_single.py
+++ testy/testyTUT/multitest/test_multiple_single.py
@@ -1,12 +1,21 @@
+import pytest
+
+@pytest.mark.number
 def test_type():
     assert type(1 + 2) is int
 
+@pytest.mark.number
 def test_add_int():
     assert 5 + 2 == 7
 
+@pytest.mark.string
 def test_string():
     assert 'u' in 'sun'
     
-
+@pytest.mark.string
 def test_add_string():
+    assert ('sunny' + 'day') == 'sunnyday'
+
+@pytest.mark.skip(reason='chwilowo off')
+def test_add_string2():
     assert ('sunny' + 'day') == 'sunnyday'
